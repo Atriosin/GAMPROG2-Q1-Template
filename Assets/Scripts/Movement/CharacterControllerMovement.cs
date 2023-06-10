c3def1b105719b351d98a2cad5ff92f643d4e68a
Floof-i <116511585+Floof-i@users.noreply.github.com>
2023-06-10 10:42:02 +0800

    added jump

--- Assets/Scripts/Movement/CharacterControllerMovement.cs
+++ Assets/Scripts/Movement/CharacterControllerMovement.cs
@@ -9,30 +9,70 @@ public class CharacterControllerMovement : MonoBehaviour
     [SerializeField]
     private float gravityScale = 1.0f;
 
+    [SerializeField]
+    private float playerHeight;
+    [SerializeField]
+    private float jumpPower = 1.0f;
+
+    private Vector3 playerVelocity;
+    public LayerMask whatIsGround;
+
+    [SerializeField]
+    private bool grounded;
+
+    bool readyToJump;
+
     private float gravity = -9.8f;
+    private float negative = -1.0f;
 
     private CharacterController characterController;
 
     private void Awake()
     {
         characterController = GetComponent<CharacterController>();
+        readyToJump = true;
     }
 
     private void Update()
     {
         Move();
+        Debug.Log(grounded);
     }
 
     private void Move()
     {
-       float xMove = Input.GetAxis("Horizontal");
-       float zMove = Input.GetAxis("Vertical");
+        float xMove = Input.GetAxis("Horizontal");
+        float zMove = Input.GetAxis("Vertical");
+
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
 
         Vector3 moveDirection = (transform.right * xMove) + (transform.forward * zMove);
         moveDirection.y += gravity * Time.deltaTime * gravityScale;
         moveDirection *= moveSpeed * Time.deltaTime;
-       
+
+        if (Input.GetKey(KeyCode.Space) && readyToJump && grounded)
+        {
+            Debug.Log("We jumped homie.");
+            readyToJump = false;
+
+            Jump();
+        }
+        
+        if (grounded)
+        {
+            readyToJump = true;
+        }
+
         //Debug.Log(moveDirection);
         characterController.Move(moveDirection);
     }
+
+    private void Jump()
+    {
+        playerVelocity.y += Mathf.Sqrt(jumpPower * negative * gravity);
+        readyToJump = false;
+        playerVelocity.y += gravity * Time.deltaTime;
+        characterController.Move(playerVelocity * Time.deltaTime);
+    }
+
 }
--- Assets/Scripts/Movement/RigidbodyMovement.cs
+++ Assets/Scripts/Movement/RigidbodyMovement.cs
@@ -8,11 +8,27 @@ public class RigidbodyMovement : MonoBehaviour
 
     [SerializeField]
     private float speed = 5.0f;
+    [SerializeField]
+    private float groundDrag = 5.0f;
+    [SerializeField]
+    private float speedModifier = 10.0f;
+    [SerializeField]
+    private float jumpPower = 0.6f;
+    [SerializeField]
+    private float airMultiplier = 0.5f;
+    [SerializeField]
+    private float playerHeight = 2.0f;
+
+    public LayerMask whatIsGround;
+    private bool grounded;
+
+    private float horizontalInput, verticalInput;
+    private Vector3 moveDirection;
+
+    public Transform orientation;
 
     //[SerializeField]
     //private int updateCount, fixedUpdateCount;
-
-    private float xMove, zMove;
 
     private void Awake()
     {
@@ -22,16 +38,67 @@ public class RigidbodyMovement : MonoBehaviour
 
     private void Update()
     {
-        //Get the input values from the player
-        xMove = Input.GetAxis("Horizontal") * speed;
-        zMove = Input.GetAxis("Vertical") * speed;
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+
+        MyInput();
+        SpeedControl();
+
+        if(grounded)
+        {
+            rb.drag = groundDrag;
+        }
+        else
+        {
+            rb.drag = 0;
+        }
     }
 
     private void FixedUpdate()
     {
-        //Change the velocity of our player
-        //rb.velocity = new Vector3(xMove, rb.velocity.y, zMove);
-        Vector3 movement = (transform.forward * zMove) + (transform.right * xMove);
-        rb.velocity = movement;
+       MovePlayer();
+    }
+
+    private void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        if(Input.GetKey(KeyCode.Space) && grounded)
+        {
+            Jump();
+        }
+    }
+
+    private void MovePlayer()
+    {
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        if(grounded)
+        {
+            rb.AddForce(moveDirection.normalized * speed * speedModifier, ForceMode.Force);
+        }
+        else if(!grounded)
+        {
+            rb.AddForce(moveDirection.normalized * speed * speedModifier * airMultiplier, ForceMode.Force);
+        }
+        
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        if(flatVel.magnitude > speed)
+        {
+            Vector3 limitedVel = flatVel.normalized * speed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        rb.AddForce(transform.up * jumpPower, ForceMode.Impulse);
     }
 }
